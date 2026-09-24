60a8e109fb7e1d6823cf84fa3abf10714542c366
Neil <neil.djebali@ynov.com>
2026-09-24 15:18:29 +0200

    Exam-live-coding

--- Coding.go
+++ Coding.go
@@ -1,1 +1,62 @@
-test
+package main
+
+import "fmt"
+
+type Basket struct{
+	name	string
+	color	string
+	content	[]Clothing
+}
+
+type Clothing struct{
+	name	string
+	color	string
+	state	string
+}
+
+func main(){
+	PanierBlanc:= Basket{
+		name: "panier blanc",
+		color: "blanc",
+		content: []Clothing{
+			content(VetementsBlanc)
+		},
+	}
+	PanierNoir:= Basket{
+		name: "panier noir",
+		color: "noir",
+		content: []Clothing{
+			content(VetementsNoir)
+		},
+	}
+	PanierCouleur:= Basket{
+		name: "panier couleur",
+		color: "couleur",
+		content: []Clothing{
+			content(VetementsCouleur)
+		},
+	}
+	VetementsBlanc:= Clothing{
+		name: "pantalon blanc",
+		color: "blanc",
+		state:	"propre",
+	}
+	VetementsNoir:= Clothing{
+		name: "pantalon noir",
+		color: "noir",
+		state:	"propre",
+	}
+	VetementsCouleur:= Clothing{
+		name: "pantalon couleur",
+		color: "couleur",
+		state:	"propre",
+	}
+}
+
+func (d Basket) displayBasket() {
+	fmt.println("=========== Panier blanc ===========")
+	fmt.println(d.panierBlanc)
+}
+
+
+
